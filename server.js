24a91c68663738a12f89a08d633a5ffc9bfba7e9
granturismo89 <bhibbard89@gmail.com>
2022-09-12 22:49:02 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,4 @@
-require('dotenv').config();
-
 const tmi = require('tmi.js');
-
-const regexpCommand = new RegExp(/^([a-zA-Z0-9]+)(?:\W+)?(.*)?/);
-
-const commands = {
-  test: {
-    response: 'test'
-  },
-  upvote: {
-    response: (argument) => `Successfully upvoted ${argument}`
-  }
-}
 
 const client = new tmi.Client({
   connection: {
@@ -19,33 +6,15 @@ const client = new tmi.Client({
   },
   channels: [
     'granturismo89'
-  ],
-  identity: {
-    username: process.env.TWITCH_BOT_USERNAME,
-    password: process.env.TWITCH_OAUTH_TOKEN
-  }
+  ]
 });
 
 client.connect();
 
 client.on('message', async (channel, context, message) => {
-  const isNotBot = context.username.toLowerCase() !== process.env.TWITCH_BOT_USERNAME.toLowerCase();
-
-  if ( !isNotBot ) return;
-
-  const {raw, command, argument} = message.match(regexpCommand);
-
-  const { response } = commands[command] || {};
-
-  let responseMessage = response;
-
-  if ( typeof responseMessage === 'function' ) {
-    responseMessage = response(argument);
-  }
-
-  if ( responseMessage ) {
-    console.log(`Responding to command !${command}`);
-    client.say(channel, responseMessage);
-  }
-
+  console.log('channel', {
+    channel,
+    user: context.username,
+    message
+  });
 });
