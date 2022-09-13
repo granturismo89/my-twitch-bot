41d02fd0d00af24e10c8fff4d04ef66d6eaab517
granturismo89 <bhibbard89@gmail.com>
2022-09-13 08:20:36 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,49 +1,25 @@
-require('dotenv').config();
-
 const tmi = require('tmi.js');
 
-const regexpCommand = new RegExp(/^!([a-zA-Z0-9]+)(?:\W+)?(.*)?/);
-
-const commands = {
-  website: {
-    response: 'https://spacejelly.dev'
-  },
-  upvote: {
-    response: (argument) => `Successfully upvoted ${argument}`
-  }
-}
-
 const client = new tmi.Client({
+  options: { debug: true },
   connection: {
+    secure: true,
     reconnect: true
   },
-  channels: [
-    'granturismo89'
-  ],
   identity: {
-    username: process.env.TWITCH_USERNAME,
+    username: 'gt89bot',
     password: process.env.TWITCH_OAUTH
-  }
+  },
+  channels: ['granturismo89']
 });
 
 client.connect();
 
-client.on('message', async (channel, context, message) => {
-  const isNotBot = context.username.toLowerCase() !== process.env.TWITCH_USERNAME.toLowerCase();
+client.on('message', (channel, tags, message, self) => {
+  // Ignore echoed messages.
+  if(self) return;
 
-  if ( !isNotBot ) return;
-
-  const { response } = commands[command] || {};
-
-  let responseMessage = response;
-
-  if ( typeof responseMessage === 'function' ) {
-    responseMessage = response(argument);
+  if(message.toLowerCase() === '!hello') {
+    client.say(channel, `@${tags.username}, Yo what's up`);
   }
-
-  if ( responseMessage ) {
-    console.log(`Responding to command !${command}`);
-    client.say(channel, responseMessage);
-  }
-
 });
