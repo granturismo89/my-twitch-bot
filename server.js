c92b61528460f5790364275e4c6ad3cb1c82a8ea
granturismo89 <bhibbard89@gmail.com>
2022-09-14 09:19:15 -0700

    Update server.js

--- server.js
+++ server.js
@@ -19,95 +19,39 @@ client.on('message', (channel, tags, message, self) => {
   // Ignore echoed messages.
   if(self) return;
 
-  if(message.toLowerCase() === 'betting is open for red vs green. use !bet [amount] [team] to place a wager!') {
+  	 if(message.toLowerCase() === 'betting is open for blue vs red. use !bet [amount] [team] to place a wager!') {
     setTimeout(() => {client.say(channel, `!betf red`); }, 8000);
   }
   
-   if(message.toLowerCase() === 'betting is open for red vs blue. use !bet [amount] [team] to place a wager!') {
-    setTimeout(() => {client.say(channel, `!betf red`); }, 8000);
-  }
-  
-     if(message.toLowerCase() === 'betting is open for red vs yellow. use !bet [amount] [team] to place a wager!') {
-    setTimeout(() => {client.say(channel, `!betf red`); }, 8000);
-  }
-  
-     if(message.toLowerCase() === 'betting is open for red vs black. use !bet [amount] [team] to place a wager!') {
-    setTimeout(() => {client.say(channel, `!betf red`); }, 8000);
-  }
-  
-     if(message.toLowerCase() === 'betting is open for red vs white. use !bet [amount] [team] to place a wager!') {
-    setTimeout(() => {client.say(channel, ` !betf red`); }, 8000);
-  }
-  
-       if(message.toLowerCase() === 'betting is open for red vs purple. use !bet [amount] [team] to place a wager!') {
-    setTimeout(() => {client.say(channel, `!betf red`); }, 8000);
-  }
-  
-       if(message.toLowerCase() === 'betting is open for red vs brown. use !bet [amount] [team] to place a wager!') {
-    setTimeout(() => {client.say(channel, `!betf red`); }, 8000);
-  }
-  
-    if(message.toLowerCase() === 'betting is open for green vs red. use !bet [amount] [team] to place a wager!') {
+   if(message.toLowerCase() === 'betting is open for blue vs green. use !bet [amount] [team] to place a wager!') {
     setTimeout(() => {client.say(channel, `!betf green`); }, 8000);
   }
   
-   if(message.toLowerCase() === 'betting is open for green vs blue. use !bet [amount] [team] to place a wager!') {
-    setTimeout(() => {client.say(channel, `!betf green`); }, 8000);
+     if(message.toLowerCase() === 'betting is open for blue vs yellow. use !bet [amount] [team] to place a wager!') {
+    setTimeout(() => {client.say(channel, `!betf yellow`); }, 8000);
   }
   
-     if(message.toLowerCase() === 'betting is open for green vs yellow. use !bet [amount] [team] to place a wager!') {
-    setTimeout(() => {client.say(channel, `!betf green`); }, 8000);
-  }
-  
-     if(message.toLowerCase() === 'betting is open for green vs black. use !bet [amount] [team] to place a wager!') {
-    setTimeout(() => {client.say(channel, `!betf green`); }, 8000);
-  }
-  
-     if(message.toLowerCase() === 'betting is open for green vs white. use !bet [amount] [team] to place a wager!') {
-    setTimeout(() => {client.say(channel, `!betf green`); }, 8000);
-  }
-  
-       if(message.toLowerCase() === 'betting is open for green vs purple. use !bet [amount] [team] to place a wager!') {
-    setTimeout(() => {client.say(channel, `!betf green`); }, 8000);
-  }
-  
-       if(message.toLowerCase() === 'betting is open for green vs brown. use !bet [amount] [team] to place a wager!') {
-    setTimeout(() => {client.say(channel, `!betf green`); }, 8000);
-  }
-  
-    if(message.toLowerCase() === 'betting is open for white vs green. use !bet [amount] [team] to place a wager!') {
+     if(message.toLowerCase() === 'betting is open for blue vs white. use !bet [amount] [team] to place a wager!') {
     setTimeout(() => {client.say(channel, `!betf white`); }, 8000);
   }
   
-   if(message.toLowerCase() === 'betting is open for white vs blue. use !bet [amount] [team] to place a wager!') {
-    setTimeout(() => {client.say(channel, `!betf white`); }, 8000);
+     if(message.toLowerCase() === 'betting is open for blue vs black. use !bet [amount] [team] to place a wager!') {
+    setTimeout(() => {client.say(channel, `!betf black`); }, 8000);
   }
   
-     if(message.toLowerCase() === 'betting is open for white vs yellow. use !bet [amount] [team] to place a wager!') {
-    setTimeout(() => {client.say(channel, `!betf white`); }, 8000);
-  }
-  
-     if(message.toLowerCase() === 'betting is open for white vs black. use !bet [amount] [team] to place a wager!') {
-    setTimeout(() => {client.say(channel, `!betf white`); }, 8000);
-  }
-  
-     if(message.toLowerCase() === 'betting is open for white vs white. use !bet [amount] [team] to place a wager!') {
-    setTimeout(() => {client.say(channel, `!betf white`); }, 8000);
-  }
-  
-       if(message.toLowerCase() === 'betting is open for white vs purple. use !bet [amount] [team] to place a wager!') {
-    setTimeout(() => {client.say(channel, `!betf white`); }, 8000);
-  }
-  
-       if(message.toLowerCase() === 'betting is open for white vs brown. use !bet [amount] [team] to place a wager!') {
-    setTimeout(() => {client.say(channel, `!betf white`); }, 8000);
-  }
-  
-    if(message.toLowerCase() === 'betting is open for purple vs red. use !bet [amount] [team] to place a wager!') {
+       if(message.toLowerCase() === 'betting is open for blue vs purple. use !bet [amount] [team] to place a wager!') {
     setTimeout(() => {client.say(channel, `!betf purple`); }, 8000);
   }
   
-   if(message.toLowerCase() === 'betting is open for purple vs blue. use !bet [amount] [team] to place a wager!') {
+       if(message.toLowerCase() === 'betting is open for blue vs brown. use !bet [amount] [team] to place a wager!') {
+    setTimeout(() => {client.say(channel, `!betf brown`); }, 8000);
+  }
+  
+  	 if(message.toLowerCase() === 'betting is open for purple vs blue. use !bet [amount] [team] to place a wager!') {
+    setTimeout(() => {client.say(channel, `!betf purple`); }, 8000);
+  }
+  
+   if(message.toLowerCase() === 'betting is open for purple vs green. use !bet [amount] [team] to place a wager!') {
     setTimeout(() => {client.say(channel, `!betf purple`); }, 8000);
   }
   
@@ -115,15 +59,15 @@ client.on('message', (channel, tags, message, self) => {
     setTimeout(() => {client.say(channel, `!betf purple`); }, 8000);
   }
   
+     if(message.toLowerCase() === 'betting is open for purple vs white. use !bet [amount] [team] to place a wager!') {
+    setTimeout(() => {client.say(channel, `!betf purple`); }, 8000);
+  }
+  
      if(message.toLowerCase() === 'betting is open for purple vs black. use !bet [amount] [team] to place a wager!') {
     setTimeout(() => {client.say(channel, `!betf purple`); }, 8000);
   }
   
-     if(message.toLowerCase() === 'betting is open for purple vs green. use !bet [amount] [team] to place a wager!') {
-    setTimeout(() => {client.say(channel, `!betf purple`); }, 8000);
-  }
-  
-       if(message.toLowerCase() === 'betting is open for purple vs white. use !bet [amount] [team] to place a wager!') {
+       if(message.toLowerCase() === 'betting is open for purple vs red. use !bet [amount] [team] to place a wager!') {
     setTimeout(() => {client.say(channel, `!betf purple`); }, 8000);
   }
   
@@ -131,60 +75,173 @@ client.on('message', (channel, tags, message, self) => {
     setTimeout(() => {client.say(channel, `!betf purple`); }, 8000);
   }
   
-    if(message.toLowerCase() === 'betting is open for blue vs red. use !bet [amount] [team] to place a wager!') {
-    setTimeout(() => {client.say(channel, `!betf blue`); }, 8000);
+  	 if(message.toLowerCase() === 'betting is open for black vs blue. use !bet [amount] [team] to place a wager!') {
+    setTimeout(() => {client.say(channel, `!betf black`); }, 8000);
   }
   
-   if(message.toLowerCase() === 'betting is open for blue vs white. use !bet [amount] [team] to place a wager!') {
-    setTimeout(() => {client.say(channel, `!betf blue`); }, 8000);
+   if(message.toLowerCase() === 'betting is open for black vs green. use !bet [amount] [team] to place a wager!') {
+    setTimeout(() => {client.say(channel, `!betf black`); }, 8000);
   }
   
-     if(message.toLowerCase() === 'betting is open for blue vs yellow. use !bet [amount] [team] to place a wager!') {
-    setTimeout(() => {client.say(channel, `!betf blue`); }, 8000);
+     if(message.toLowerCase() === 'betting is open for black vs yellow. use !bet [amount] [team] to place a wager!') {
+    setTimeout(() => {client.say(channel, `!betf black`); }, 8000);
   }
   
-     if(message.toLowerCase() === 'betting is open for blue vs black. use !bet [amount] [team] to place a wager!') {
-    setTimeout(() => {client.say(channel, `!betf blue`); }, 8000);
+     if(message.toLowerCase() === 'betting is open for black vs white. use !bet [amount] [team] to place a wager!') {
+    setTimeout(() => {client.say(channel, `!betf black`); }, 8000);
   }
   
-     if(message.toLowerCase() === 'betting is open for blue vs green. use !bet [amount] [team] to place a wager!') {
-    setTimeout(() => {client.say(channel, `!betf blue`); }, 8000);
+     if(message.toLowerCase() === 'betting is open for black vs black. use !bet [amount] [team] to place a wager!') {
+    setTimeout(() => {client.say(channel, `!betf black`); }, 8000);
   }
   
-       if(message.toLowerCase() === 'betting is open for blue vs blue. use !bet [amount] [team] to place a wager!') {
-    setTimeout(() => {client.say(channel, `!betf blue`); }, 8000);
+       if(message.toLowerCase() === 'betting is open for black vs purple. use !bet [amount] [team] to place a wager!') {
+    setTimeout(() => {client.say(channel, `!betf purple`); }, 8000);
   }
   
-       if(message.toLowerCase() === 'betting is open for blue vs brown. use !bet [amount] [team] to place a wager!') {
-    setTimeout(() => {client.say(channel, `!betf blue`); }, 8000);
+       if(message.toLowerCase() === 'betting is open for black vs brown. use !bet [amount] [team] to place a wager!') {
+    setTimeout(() => {client.say(channel, `!betf black`); }, 8000);
   }
   
-  if(message.toLowerCase() === 'betting is open for yellow vs red. use !bet [amount] [team] to place a wager!') {
+  	 if(message.toLowerCase() === 'betting is open for white vs blue. use !bet [amount] [team] to place a wager!') {
+    setTimeout(() => {client.say(channel, `!betf white`); }, 8000);
+  }
+  
+   if(message.toLowerCase() === 'betting is open for white vs green. use !bet [amount] [team] to place a wager!') {
+    setTimeout(() => {client.say(channel, `!betf white`); }, 8000);
+  }
+  
+     if(message.toLowerCase() === 'betting is open for white vs yellow. use !bet [amount] [team] to place a wager!') {
+    setTimeout(() => {client.say(channel, `!betf white`); }, 8000);
+  }
+  
+     if(message.toLowerCase() === 'betting is open for white vs white. use !bet [amount] [team] to place a wager!') {
+    setTimeout(() => {client.say(channel, `!betf white`); }, 8000);
+  }
+  
+     if(message.toLowerCase() === 'betting is open for white vs black. use !bet [amount] [team] to place a wager!') {
+    setTimeout(() => {client.say(channel, `!betf black`); }, 8000);
+  }
+  
+       if(message.toLowerCase() === 'betting is open for white vs purple. use !bet [amount] [team] to place a wager!') {
+    setTimeout(() => {client.say(channel, `!betf purple`); }, 8000);
+  }
+  
+       if(message.toLowerCase() === 'betting is open for white vs brown. use !bet [amount] [team] to place a wager!') {
+    setTimeout(() => {client.say(channel, `!betf white`); }, 8000);
+  }
+  
+  	 if(message.toLowerCase() === 'betting is open for green vs blue. use !bet [amount] [team] to place a wager!') {
+    setTimeout(() => {client.say(channel, `!betf green`); }, 8000);
+  }
+  
+   if(message.toLowerCase() === 'betting is open for green vs green. use !bet [amount] [team] to place a wager!') {
+    setTimeout(() => {client.say(channel, `!betf green`); }, 8000);
+  }
+  
+     if(message.toLowerCase() === 'betting is open for green vs yellow. use !bet [amount] [team] to place a wager!') {
+    setTimeout(() => {client.say(channel, `!betf green`); }, 8000);
+  }
+  
+     if(message.toLowerCase() === 'betting is open for green vs white. use !bet [amount] [team] to place a wager!') {
+    setTimeout(() => {client.say(channel, `!betf white`); }, 8000);
+  }
+  
+     if(message.toLowerCase() === 'betting is open for green vs black. use !bet [amount] [team] to place a wager!') {
+    setTimeout(() => {client.say(channel, `!betf black`); }, 8000);
+  }
+  
+       if(message.toLowerCase() === 'betting is open for green vs purple. use !bet [amount] [team] to place a wager!') {
+    setTimeout(() => {client.say(channel, `!betf purple`); }, 8000);
+  }
+  
+       if(message.toLowerCase() === 'betting is open for green vs brown. use !bet [amount] [team] to place a wager!') {
+    setTimeout(() => {client.say(channel, `!betf brown`); }, 8000);
+  }
+  
+  	 if(message.toLowerCase() === 'betting is open for yellow vs blue. use !bet [amount] [team] to place a wager!') {
     setTimeout(() => {client.say(channel, `!betf yellow`); }, 8000);
   }
   
-   if(message.toLowerCase() === 'betting is open for yellow vs white. use !bet [amount] [team] to place a wager!') {
+   if(message.toLowerCase() === 'betting is open for yellow vs green. use !bet [amount] [team] to place a wager!') {
+    setTimeout(() => {client.say(channel, `!betf green`); }, 8000);
+  }
+  
+     if(message.toLowerCase() === 'betting is open for yellow vs yellow. use !bet [amount] [team] to place a wager!') {
     setTimeout(() => {client.say(channel, `!betf yellow`); }, 8000);
   }
   
-     if(message.toLowerCase() === 'betting is open for yellow vs blue. use !bet [amount] [team] to place a wager!') {
-    setTimeout(() => {client.say(channel, `!betf yellow`); }, 8000);
+     if(message.toLowerCase() === 'betting is open for yellow vs white. use !bet [amount] [team] to place a wager!') {
+    setTimeout(() => {client.say(channel, `!betf white`); }, 8000);
   }
   
      if(message.toLowerCase() === 'betting is open for yellow vs black. use !bet [amount] [team] to place a wager!') {
-    setTimeout(() => {client.say(channel, `!betf yellow`); }, 8000);
-  }
-  
-     if(message.toLowerCase() === 'betting is open for yellow vs green. use !bet [amount] [team] to place a wager!') {
-    setTimeout(() => {client.say(channel, `!betf yellow`); }, 8000);
+    setTimeout(() => {client.say(channel, `!betf black`); }, 8000);
   }
   
        if(message.toLowerCase() === 'betting is open for yellow vs purple. use !bet [amount] [team] to place a wager!') {
-    setTimeout(() => {client.say(channel, `!betf yellow`); }, 8000);
+    setTimeout(() => {client.say(channel, `!betf purple`); }, 8000);
   }
   
        if(message.toLowerCase() === 'betting is open for yellow vs brown. use !bet [amount] [team] to place a wager!') {
-    setTimeout(() => {client.say(channel, `!betf yellow`); }, 8000);
+    setTimeout(() => {client.say(channel, `!betf brown`); }, 8000);
+  }
+  
+  
+  	 if(message.toLowerCase() === 'betting is open for brown vs blue. use !bet [amount] [team] to place a wager!') {
+    setTimeout(() => {client.say(channel, `!betf brown`); }, 8000);
+  }
+  
+   if(message.toLowerCase() === 'betting is open for brown vs green. use !bet [amount] [team] to place a wager!') {
+    setTimeout(() => {client.say(channel, `!betf brown`); }, 8000);
+  }
+  
+     if(message.toLowerCase() === 'betting is open for brown vs yellow. use !bet [amount] [team] to place a wager!') {
+    setTimeout(() => {client.say(channel, `!betf brown`); }, 8000);
+  }
+  
+     if(message.toLowerCase() === 'betting is open for brown vs white. use !bet [amount] [team] to place a wager!') {
+    setTimeout(() => {client.say(channel, `!betf white`); }, 8000);
+  }
+  
+     if(message.toLowerCase() === 'betting is open for brown vs black. use !bet [amount] [team] to place a wager!') {
+    setTimeout(() => {client.say(channel, `!betf black`); }, 8000);
+  }
+  
+       if(message.toLowerCase() === 'betting is open for brown vs purple. use !bet [amount] [team] to place a wager!') {
+    setTimeout(() => {client.say(channel, `!betf purple`); }, 8000);
+  }
+  
+       if(message.toLowerCase() === 'betting is open for brown vs brown. use !bet [amount] [team] to place a wager!') {
+    setTimeout(() => {client.say(channel, `!betf brown`); }, 8000);
+  }
+  
+  	 if(message.toLowerCase() === 'betting is open for red vs blue. use !bet [amount] [team] to place a wager!') {
+    setTimeout(() => {client.say(channel, `!betf red`); }, 8000);
+  }
+  
+   if(message.toLowerCase() === 'betting is open for red vs green. use !bet [amount] [team] to place a wager!') {
+    setTimeout(() => {client.say(channel, `!betf red`); }, 8000);
+  }
+  
+     if(message.toLowerCase() === 'betting is open for red vs yellow. use !bet [amount] [team] to place a wager!') {
+    setTimeout(() => {client.say(channel, `!betf red`); }, 8000);
+  }
+  
+     if(message.toLowerCase() === 'betting is open for red vs white. use !bet [amount] [team] to place a wager!') {
+    setTimeout(() => {client.say(channel, `!betf white`); }, 8000);
+  }
+  
+     if(message.toLowerCase() === 'betting is open for red vs black. use !bet [amount] [team] to place a wager!') {
+    setTimeout(() => {client.say(channel, `!betf black`); }, 8000);
+  }
+  
+       if(message.toLowerCase() === 'betting is open for red vs purple. use !bet [amount] [team] to place a wager!') {
+    setTimeout(() => {client.say(channel, `!betf purple`); }, 8000);
+  }
+  
+       if(message.toLowerCase() === 'betting is open for red vs brown. use !bet [amount] [team] to place a wager!') {
+    setTimeout(() => {client.say(channel, `!betf brown`); }, 8000);
   }
   
 });
